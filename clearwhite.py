6abc997d13feaa8a2e374cbb4415fddac7b767c5
tademarest <tyson.demarest@gmail.com>
2012-07-03 20:32:15 -0300

    Update master

--- clearwhite.py
+++ clearwhite.py
@@ -4,19 +4,36 @@ from gimpfu import *
 
 def plugin_clearwhite(timg, tdrawable):
 
-
-#if corner is white proceed
-    topleft=list(pdb.gimp_image_pick_color(timg,tdrawable,0,0,0,1,5)) #assigns rgba list avg color () within radius 5 px from 0,0
-    if (sum(topleft[0:3])>720) : #sums red green and blue, proceed if this value is "white" enough
-
-        pdb.gimp_fuzzy_select(tdrawable,0,0,100,2,1,0,0,0) # fuzzy wand selects from the upper left corner
-        pdb.gimp_layer_add_alpha(timg.active_layer) #adds an alpha layer (if necessary)
-        pdb.gimp_edit_clear(tdrawable) #clears the selected, ie makes selected transparent
+    #proceed if topleft corner is white
+    topleft=list(pdb.gimp_image_pick_color(timg,tdrawable,0,0,0,1,3)) #assigns RGBA list avg color () within radius 5 px from 0,0
+	if ((sum(topleft[0:3])>745) & (topleft[3]>10)) : #sums red green and blue, proceed if this value is "white" enough, checks transparecy
+       		pdb.gimp_fuzzy_select(tdrawable,0,0,15,2,1,0,0,0) # fuzzy wand selects from the upper left corner
+        	pdb.gimp_edit_clear(tdrawable) #clears the selected, ie makes selected transparent
+		    pdb.gimp_selection_none(timg) #deselect all
 
 
-    pdb.plug_in_autocrop(timg,tdrawable) #crops out the extra stuff
+    #proceed if topright corner is white
+	topright=list(pdb.gimp_image_pick_color(timg,tdrawable,(tdrawable.width-1),0,0,1,3)) #same at tdrawable.width,0
+	if ((sum(topright[0:3])>745) & (topright[3]>10)) : #sums red green and blue, proceed if this value is "white" enough
+       		pdb.gimp_fuzzy_select(tdrawable,(tdrawable.width-1),0,15,2,1,0,0,0) # fuzzy wand selects from the upper right corner
+        	pdb.gimp_edit_clear(tdrawable) #clears the selected, ie makes selected transparent
+		    pdb.gimp_selection_none(timg) #deselect all
 
-#savesas png in new folder
+    #proceed if bottomleft corner is white
+	bottomleft=list(pdb.gimp_image_pick_color(timg,tdrawable,0,(tdrawable.height-1),0,1,3)) #same at 0,tdrawable.height
+	if ((sum(bottomleft[0:3])>745) & (bottomleft[3]>10)) : #sums red green and blue, proceed if this value is "white" enough
+       		pdb.gimp_fuzzy_select(tdrawable,0,(tdrawable.height-1),15,2,1,0,0,0) # fuzzy wand selects from the bottom left corner
+        	pdb.gimp_edit_clear(tdrawable) #clears the selected, ie makes selected transparent
+		    pdb.gimp_selection_none(timg) #deselect all
+
+    #proceed if bottom rightcorner is white
+	bottomright=list(pdb.gimp_image_pick_color(timg,tdrawable,(tdrawable.width-1),(tdrawable.height-1),0,1,3)) #same at width,height
+	if ((sum(bottomright[0:3])>745) & (bottomright[3]>10)) : #sums red green and blue, proceed if this value is "white" enough
+       		pdb.gimp_fuzzy_select(tdrawable,(tdrawable.width-1),(tdrawable.height-1),15,2,1,0,0,0) # wand selects from bottom right corner
+        	pdb.gimp_edit_clear(tdrawable) #clears the selected, ie makes selected transparent
+		    pdb.gimp_selection_none(timg) #deselect all
+
+    #savesas png in new folder
     pdb.file_png_save2(timg, tdrawable, "/home/ty/newlogos/"+timg.name+".png", timg.name+".png", 0, 0, 0, 0, 0, 0, 0, 0, 1)
 
 register(
