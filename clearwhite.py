2b34a7478283d4529f5b4dcf9e3cdc7396f8eb73
tademarest <tyson.demarest@gmail.com>
2012-07-03 20:32:53 -0300

    Update master

--- clearwhite.py
+++ clearwhite.py
@@ -9,7 +9,7 @@ def plugin_clearwhite(timg, tdrawable):
 	if ((sum(topleft[0:3])>745) & (topleft[3]>10)) : #sums red green and blue, proceed if this value is "white" enough, checks transparecy
        		pdb.gimp_fuzzy_select(tdrawable,0,0,15,2,1,0,0,0) # fuzzy wand selects from the upper left corner
         	pdb.gimp_edit_clear(tdrawable) #clears the selected, ie makes selected transparent
-		    pdb.gimp_selection_none(timg) #deselect all
+		pdb.gimp_selection_none(timg) #deselect all
 
 
     #proceed if topright corner is white
@@ -17,21 +17,21 @@ def plugin_clearwhite(timg, tdrawable):
 	if ((sum(topright[0:3])>745) & (topright[3]>10)) : #sums red green and blue, proceed if this value is "white" enough
        		pdb.gimp_fuzzy_select(tdrawable,(tdrawable.width-1),0,15,2,1,0,0,0) # fuzzy wand selects from the upper right corner
         	pdb.gimp_edit_clear(tdrawable) #clears the selected, ie makes selected transparent
-		    pdb.gimp_selection_none(timg) #deselect all
+		pdb.gimp_selection_none(timg) #deselect all
 
     #proceed if bottomleft corner is white
 	bottomleft=list(pdb.gimp_image_pick_color(timg,tdrawable,0,(tdrawable.height-1),0,1,3)) #same at 0,tdrawable.height
 	if ((sum(bottomleft[0:3])>745) & (bottomleft[3]>10)) : #sums red green and blue, proceed if this value is "white" enough
        		pdb.gimp_fuzzy_select(tdrawable,0,(tdrawable.height-1),15,2,1,0,0,0) # fuzzy wand selects from the bottom left corner
         	pdb.gimp_edit_clear(tdrawable) #clears the selected, ie makes selected transparent
-		    pdb.gimp_selection_none(timg) #deselect all
+		pdb.gimp_selection_none(timg) #deselect all
 
     #proceed if bottom rightcorner is white
 	bottomright=list(pdb.gimp_image_pick_color(timg,tdrawable,(tdrawable.width-1),(tdrawable.height-1),0,1,3)) #same at width,height
 	if ((sum(bottomright[0:3])>745) & (bottomright[3]>10)) : #sums red green and blue, proceed if this value is "white" enough
        		pdb.gimp_fuzzy_select(tdrawable,(tdrawable.width-1),(tdrawable.height-1),15,2,1,0,0,0) # wand selects from bottom right corner
         	pdb.gimp_edit_clear(tdrawable) #clears the selected, ie makes selected transparent
-		    pdb.gimp_selection_none(timg) #deselect all
+		pdb.gimp_selection_none(timg) #deselect all
 
     #savesas png in new folder
     pdb.file_png_save2(timg, tdrawable, "/home/ty/newlogos/"+timg.name+".png", timg.name+".png", 0, 0, 0, 0, 0, 0, 0, 0, 1)
